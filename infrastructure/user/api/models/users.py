f136b5f704be2722e292749ec3698adb8355b049
wesleycurldev <wesley@curldev.com.br>
2023-01-21 22:00:24 -0300

    add SQLAlchemy instance and model Users with database conection

---
+++ infrastructure/user/api/models/users.py
@@ -0,0 +1,9 @@
+import uuid
+from .db import db
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class Users(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80))
