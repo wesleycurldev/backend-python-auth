3d76ae8e812ca6d5e7695a2d3efd53758680433d
wesleycurldev <wesley@curldev.com.br>
2023-01-21 22:01:39 -0300

    add external user repository

---
+++ infrastructure/user/api/repositories/users.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.path.append('..')
+sys.path.append('../../..')
+
+from auth_service.application.user.user_storage import UserStorage
+from auth_service.application.user.user_dto import UserDto
+from auth_service.application.user.user_services import UserService
+from models.users import Users
+
+class UserRepository(UserStorage):
+    def get_user_by_username(self, username) -> UserDto:
+        return Users.query.filter_by(username=username).first()
