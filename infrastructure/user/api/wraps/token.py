efd43fe7853be66b9c34c2571e9992247c454902
wesleycurldev <wesley@curldev.com.br>
2023-01-21 22:02:43 -0300

    add token jwt decorator

---
+++ infrastructure/user/api/wraps/token.py
@@ -0,0 +1,27 @@
+from jwt import decode, ExpiredSignatureError
+
+from flask import make_response, request, session
+from functools import wraps
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+            
+            try:
+                data = decode(token, environ.get("SECRET_KEY"), algorithms=["HS256"])
+                session['current_user'] = {'user': data['user']}
+            except ExpiredSignatureError as e:
+                return make_response('Signature has expired', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+            except Exception:
+                return make_response('Invalid token', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+            
+            return f(*args, **kwargs)
+        else:
+            return make_response('Authorization is required in the header', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return decorated
